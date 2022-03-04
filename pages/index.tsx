9928ea183fc4d220903fdd2817243792daf6e89b
Ben Jung <benjungkz@gmail.com>
2022-03-03 21:50:35 -0500

    Pratice modifier on Tailwind CSS

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,13 @@ import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
   return (
-    <div className="bg-red-500">
-      <h1 className="text-black-100 text-center">Tailwind CSS is working</h1>
+    <div>
+      <h1 className="font-bold text-2xl text-center text-blue-600 hover:text-green-700 p-5">Modifier</h1>
+      <div className="flex gap-6 justify-center">
+        <button className="w-5 h-5 rounded-full bg-red-600"></button>
+        <button className="w-5 h-5 rounded-full bg-blue-600"></button>
+        <button className="w-5 h-5 rounded-full bg-yellow-300"></button>
+      </div>
     </div>
   )
 }
